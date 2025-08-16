ca2d307ac9f985860943dd33f71df111d46794c9
Orlandinho <horlandomagic@gmail.com>
2025-08-16 15:15:47 -0300

    Set SQLite as this app's database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,43 @@
 import express from 'express';
+import Database from "better-sqlite3";
 
 const app = express();
 
 const PORT = 8000;
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const db = new Database('./backend/database/data.db');
 
-app.get('/', (req, res) => {
-    res.send('Server is running well!')
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS users
+        (
+            id INTEGER PRIMARY KEY,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            created_at DATETIME NOT NULL
+        )`
+    ).run();
+
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS transactions
+        (
+            id             INTEGER PRIMARY KEY,
+            user_id        INTEGER  NOT NULL,
+            amount         INTEGER  NOT NULL,
+            currency_from  INTEGER  NOT NULL,
+            currency_to    INTEGER  NOT NULL,
+            conversion_tax INTEGER  NOT NULL,
+            created_at     DATETIME NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users (id)
+        )`
+    ).run();
+
+app.listen(PORT, () => {
+    try {
+        db.prepare('SELECT 1').get();
+        console.log('Database is working!');
+    } catch (err) {
+        console.error('Error when trying to open database: ', err);
+        process.exit(1);
+    }
+    console.log(`Server is running on port ${PORT}`)
 });
